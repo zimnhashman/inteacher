9eee2aacfc6bd038c4a23511b0bda6f4a7fb99c4
Nhaniso Undenge <nhanitech@gmail.com>
2023-06-13 05:12:40 +0200

    InTeacher initial prototype mock

---
+++ home.php
@@ -0,0 +1,11 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+<h1>Education From Home</h1>
+    <p>  Welcome to Infant school learning</p> 
+</div>
+
+</ol>
+</div>
+ 
+
---
+++ pam_db.sql
@@ -0,0 +1,314 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 30, 2022 at 11:06 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pam_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblautonumbers`
+--
+
+CREATE TABLE `tblautonumbers` (
+  `AUTOID` int(11) NOT NULL,
+  `AUTOSTART` varchar(30) NOT NULL,
+  `AUTOEND` int(11) NOT NULL,
+  `AUTOINC` int(11) NOT NULL,
+  `AUTOKEY` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblautonumbers`
+--
+
+INSERT INTO `tblautonumbers` (`AUTOID`, `AUTOSTART`, `AUTOEND`, `AUTOINC`, `AUTOKEY`) VALUES
+(1, '02983', 8, 1, 'userid'),
+(10, '000', 6, 1, 'ExerciseID'),
+(12, '000', 34, 1, 'BLOGID'),
+(13, '0', 5, 1, 'STUDENTID');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblexercise`
+--
+
+CREATE TABLE `tblexercise` (
+  `ExerciseID` int(11) NOT NULL,
+  `LessonID` int(11) NOT NULL,
+  `Question` text NOT NULL,
+  `ChoiceA` text NOT NULL,
+  `ChoiceB` text NOT NULL,
+  `ChoiceC` text NOT NULL,
+  `ChoiceD` text NOT NULL,
+  `Answer` varchar(90) NOT NULL,
+  `ExercisesDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblexercise`
+--
+
+INSERT INTO `tblexercise` (`ExerciseID`, `LessonID`, `Question`, `ChoiceA`, `ChoiceB`, `ChoiceC`, `ChoiceD`, `Answer`, `ExercisesDate`) VALUES
+(20180001, 6, 'What is the title of the video', 'My Father', 'My Mother', 'My Brother', 'My Sister', 'My Sister', '0000-00-00'),
+(20180002, 6, 'Who is the name of the character in the story?', 'Ben', 'Holly', 'Gaston', 'Wise old elf', 'Gaston', '0000-00-00'),
+(20220003, 8, 'Who is reading ', 'Tom', 'Walter', 'Karl', 'None', 'Tom', '0000-00-00'),
+(20220004, 7, 'Who created the world?', 'Father', 'God', 'Man', 'Fire', 'God', '0000-00-00'),
+(20220005, 6, 'Who the  was mother of Peter ?', 'Mary', 'Martha', 'Kundai', 'Hen', 'Mary', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbllesson`
+--
+
+CREATE TABLE `tbllesson` (
+  `LessonID` int(11) NOT NULL,
+  `LessonChapter` varchar(90) NOT NULL,
+  `LessonTitle` varchar(90) NOT NULL,
+  `FileLocation` text NOT NULL,
+  `Category` varchar(90) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbllesson`
+--
+
+INSERT INTO `tbllesson` (`LessonID`, `LessonChapter`, `LessonTitle`, `FileLocation`, `Category`) VALUES
+(6, 'video', 'video', 'files/Back to School 2020 - Supplies for School - Cartoon for Children - Kids Academy_2.mp4', 'Video'),
+(7, 'test', 'test', 'files/s.pdf', 'Docs'),
+(8, 'Reading and Writing', 'Comprehension', 'files/Dont Look Down (feat. Mos Def_ Lupe Fiasco & Big Sean).mp4', 'Video');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblscore`
+--
+
+CREATE TABLE `tblscore` (
+  `ScoreID` int(11) NOT NULL,
+  `LessonID` int(11) NOT NULL,
+  `ExerciseID` int(11) NOT NULL,
+  `StudentID` int(11) NOT NULL,
+  `NoItems` int(11) NOT NULL DEFAULT 1,
+  `Score` int(11) NOT NULL,
+  `Submitted` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblscore`
+--
+
+INSERT INTO `tblscore` (`ScoreID`, `LessonID`, `ExerciseID`, `StudentID`, `NoItems`, `Score`, `Submitted`) VALUES
+(9, 6, 20180001, 1, 1, 1, 1),
+(10, 6, 20180002, 1, 1, 1, 1),
+(11, 6, 20180001, 4, 1, 0, 1),
+(12, 6, 20180002, 4, 1, 1, 1),
+(13, 7, 20220004, 4, 1, 1, 1),
+(14, 8, 20220003, 4, 1, 1, 1),
+(15, 7, 20220004, 5, 1, 1, 1),
+(16, 6, 20180001, 5, 1, 0, 1),
+(17, 6, 20180002, 5, 1, 1, 1),
+(18, 6, 20220005, 5, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblstudent`
+--
+
+CREATE TABLE `tblstudent` (
+  `StudentID` int(11) NOT NULL,
+  `Fname` varchar(90) NOT NULL,
+  `Lname` varchar(90) NOT NULL,
+  `Address` varchar(90) NOT NULL,
+  `MobileNo` varchar(90) NOT NULL,
+  `STUDUSERNAME` varchar(90) NOT NULL,
+  `STUDPASS` varchar(90) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblstudent`
+--
+
+INSERT INTO `tblstudent` (`StudentID`, `Fname`, `Lname`, `Address`, `MobileNo`, `STUDUSERNAME`, `STUDPASS`) VALUES
+(1, 'a', 'a', 'a', '21', 'a', '86f7e437faa5a7fce15d1ddcb9eaeaea377667b8'),
+(2, 'sd', 'sad', 'sad', '231', 'a', 'a0f1490a20d0211c997b44bc357e1972deab8ae3'),
+(3, 'tarisa', 'rova', '343', '939339933', 'tarisai', '0068ef8d6f9c1a44bd252c0f29d9daa8327b5c64'),
+(4, 'luke', 'magaya', '738373', '0388388333', 'luke', '6b3799be4300e44489a08090123f3842e6419da5'),
+(5, 'mike', 'zororo', '44333', '849948949', 'mike', 'a17fed27eaa842282862ff7c1b9c8395a26ac320');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblstudentquestion`
+--
+
+CREATE TABLE `tblstudentquestion` (
+  `SQID` int(11) NOT NULL,
+  `ExerciseID` int(11) NOT NULL,
+  `LessonID` int(11) NOT NULL,
+  `StudentID` int(11) NOT NULL,
+  `Question` varchar(90) NOT NULL,
+  `CA` varchar(90) NOT NULL,
+  `CB` varchar(90) NOT NULL,
+  `CC` varchar(90) NOT NULL,
+  `CD` varchar(90) NOT NULL,
+  `QA` varchar(90) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblstudentquestion`
+--
+
+INSERT INTO `tblstudentquestion` (`SQID`, `ExerciseID`, `LessonID`, `StudentID`, `Question`, `CA`, `CB`, `CC`, `CD`, `QA`) VALUES
+(1, 20180002, 0, 1, 'Who is the name of the character in the story?', 'Ben', 'Holly', 'Gaston', 'Wise old elf', 'Gaston'),
+(2, 20180002, 0, 2, 'Who is the name of the character in the story?', 'Ben', 'Holly', 'Gaston', 'Wise old elf', 'Gaston'),
+(3, 20220003, 8, 1, 'Who is reading ', 'Tom', 'Walter', 'Karl', 'None', 'Tom'),
+(4, 20220003, 8, 2, 'Who is reading ', 'Tom', 'Walter', 'Karl', 'None', 'Tom'),
+(5, 20220003, 8, 3, 'Who is reading ', 'Tom', 'Walter', 'Karl', 'None', 'Tom'),
+(6, 20220003, 8, 4, 'Who is reading ', 'Tom', 'Walter', 'Karl', 'None', 'Tom'),
+(7, 20220004, 7, 1, 'Who created the world?', 'Father', 'God', 'Man', 'Fire', 'God'),
+(8, 20220004, 7, 2, 'Who created the world?', 'Father', 'God', 'Man', 'Fire', 'God'),
+(9, 20220004, 7, 3, 'Who created the world?', 'Father', 'God', 'Man', 'Fire', 'God'),
+(10, 20220004, 7, 4, 'Who created the world?', 'Father', 'God', 'Man', 'Fire', 'God'),
+(11, 20220005, 6, 1, 'Who the  was mother of Peter ?', 'Mary', 'Martha', 'Kundai', 'Hen', 'Mary'),
+(12, 20220005, 6, 2, 'Who the  was mother of Peter ?', 'Mary', 'Martha', 'Kundai', 'Hen', 'Mary'),
+(13, 20220005, 6, 3, 'Who the  was mother of Peter ?', 'Mary', 'Martha', 'Kundai', 'Hen', 'Mary'),
+(14, 20220005, 6, 4, 'Who the  was mother of Peter ?', 'Mary', 'Martha', 'Kundai', 'Hen', 'Mary');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblusers`
+--
+
+CREATE TABLE `tblusers` (
+  `USERID` int(11) NOT NULL,
+  `NAME` varchar(90) NOT NULL,
+  `UEMAIL` varchar(90) NOT NULL,
+  `PASS` varchar(90) NOT NULL,
+  `TYPE` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblusers`
+--
+
+INSERT INTO `tblusers` (`USERID`, `NAME`, `UEMAIL`, `PASS`, `TYPE`) VALUES
+(1, 'Pamela', 'pamela', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Administrator');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblautonumbers`
+--
+ALTER TABLE `tblautonumbers`
+  ADD PRIMARY KEY (`AUTOID`);
+
+--
+-- Indexes for table `tblexercise`
+--
+ALTER TABLE `tblexercise`
+  ADD PRIMARY KEY (`ExerciseID`);
+
+--
+-- Indexes for table `tbllesson`
+--
+ALTER TABLE `tbllesson`
+  ADD PRIMARY KEY (`LessonID`);
+
+--
+-- Indexes for table `tblscore`
+--
+ALTER TABLE `tblscore`
+  ADD PRIMARY KEY (`ScoreID`);
+
+--
+-- Indexes for table `tblstudent`
+--
+ALTER TABLE `tblstudent`
+  ADD PRIMARY KEY (`StudentID`) USING BTREE;
+
+--
+-- Indexes for table `tblstudentquestion`
+--
+ALTER TABLE `tblstudentquestion`
+  ADD PRIMARY KEY (`SQID`);
+
+--
+-- Indexes for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  ADD PRIMARY KEY (`USERID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblautonumbers`
+--
+ALTER TABLE `tblautonumbers`
+  MODIFY `AUTOID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `tblexercise`
+--
+ALTER TABLE `tblexercise`
+  MODIFY `ExerciseID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20220006;
+
+--
+-- AUTO_INCREMENT for table `tbllesson`
+--
+ALTER TABLE `tbllesson`
+  MODIFY `LessonID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tblscore`
+--
+ALTER TABLE `tblscore`
+  MODIFY `ScoreID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `tblstudent`
+--
+ALTER TABLE `tblstudent`
+  MODIFY `StudentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tblstudentquestion`
+--
+ALTER TABLE `tblstudentquestion`
+  MODIFY `SQID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  MODIFY `USERID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
